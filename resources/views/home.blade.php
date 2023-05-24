5e28af40c86e88dc28be4b01e41637c0b5689027
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-24 22:57:28 +0200

    Costum page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>home</h1>
-    <ul>
-        @foreach ($trains as $train)
-            <li> {{ $train->agency }} </li>
-            <li> {{ $train->departure_station }} </li>
-        @endforeach
-    </ul>
+    <div class="container-fluid">
+        <ul class="py-3">
+            <li class="d-flex justify-content-start text-uppercase fw-semibold">
+                <div>
+                    <p>Agency</p>
+                </div>
+                <div>
+                    <p>departure_station</p>
+                </div>
+                <div>
+                    <p>arrival_station</p>
+                </div>
+                <div>
+                    <p>departure_time</p>
+                </div>
+                <div>
+                    <p>arrival_time</p>
+                </div>
+                <div>
+                    <p>train_code</p>
+                </div>
+                <div>
+                    <p>number_of_carriages</p>
+                </div>
+                <div>
+                    <p>in_time</p>
+                </div>
+                <div>
+                    <p>deleted</p>
+                </div>
+            </li>
+            @foreach ($trains as $train)
+                <li class="d-flex justify-content-start text-white fw-semibold">
+                    <div>
+                        <p class="text-capitalize">{{ $train->agency }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->departure_station }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->arrival_station }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->departure_time }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->arrival_time }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->train_code }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->number_of_carriages }}</p>
+                    </div>
+                    <div>
+                        <p>{{ $train->in_time }}</p>
+                    </div>
+                    <div style="background-color: {{ $train->deleted ? 'green' : 'red' }}">
+                        <p class="text-capitalize {{ $train->deleted ? 'deleted' : 'not-deleted' }}">
+                            {{ $train->deleted ? 'not deleted' : 'deleted' }}
+                        </p>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header class="container-fluid">
+    <h1 class="p-2">Japan Railways</h1>
+</header>
